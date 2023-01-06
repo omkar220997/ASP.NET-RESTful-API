94d4e470945ee8da1f0ceba55bd10ff7a4a34921
omkar kadam <omkar.kadam@klingelnberg.com>
2023-01-06 12:15:23 +0530

    Separation of outer facing from the entity class

---
+++ Exercise files/ASP dot net core 3 RESTful api/CourseLibrary/CourseLibrary.API/Helpers/DateTimeOffsetExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CourseLibrary.API.Helpers
+{
+    public static class DateTimeOffserExtensions
+    {
+        public static int GetCurrentAge(this DateTimeOffset dateTimeOffset)
+        {
+            var currentDate = DateTime.UtcNow;
+            int age = currentDate.Year - dateTimeOffset.Year;
+            if(currentDate< dateTimeOffset.AddYears(age))
+            {
+                age--;
+            }
+            return age;
+        }
+    }
+}
